3c0f387bbe76e40b54793e21bbe188035ea4706b
BlairXin <peggy10232000@gmail.com>
2022-09-29 23:31:02 +0800

    linerobot

---
+++ key.py
@@ -0,0 +1,6 @@
+# Channel Access Token
+line_bot_api = '4PE0Uay7r/c+mAnnxqk2/3Kn7afRgo5uuCWn7n4aGsvVa7SR9o5MIbwkalrYl/OfWdJTbm4j/BVMa19JhtnaSMOh5C4m4Aq+BcRenv+DP30wF4beo0En1AG73Ein/ZXVzjUfUve9syK+xCVcxSZuXwdB04t89/1O/w1cDnyilFU='
+# Channel Secret
+handler = '3847f964d55ae4c8ee017a62b3d5c298'
+#heroku-postgresql URI
+URI='postgres://prfdpwzbmaeqzk:55800aaaa8f838a78644347dfd8804fb0ead014392364aff65bfcf5eb62752fd@ec2-52-2-118-38.compute-1.amazonaws.com:5432/d589pfq45qhrpn'
